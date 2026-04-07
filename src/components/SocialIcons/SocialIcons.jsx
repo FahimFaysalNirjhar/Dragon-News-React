ca0c8107ef6443bc951d1082581210b7387a09e1
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-04-07 23:40:15 +0600

    social icons added

---
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+
+const SocialIcons = () => {
+  return (
+    <div className="space-y-5">
+      <h1 className="font-bold text-xl">Login with</h1>
+      <div className="space-y-3">
+        <button className="btn btn-secondary btn-outline w-full text-lg">
+          <FcGoogle /> Login with Google
+        </button>
+        <button className="btn btn-primary btn-outline w-full text-lg">
+          <FaGithub /> Login with GitHub
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialIcons;
--- src/pagees/RightAside/RightAside.jsx
+++ src/pagees/RightAside/RightAside.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SocialIcons from "../../components/SocialIcons/SocialIcons";
 
 const RightAside = () => {
-  return <div></div>;
+  return (
+    <div>
+      <SocialIcons></SocialIcons>
+    </div>
+  );
 };
 
 export default RightAside;
