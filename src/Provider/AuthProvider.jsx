72d2e78b6225ea73d087f7d74b6f5c95347f0907
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-04-12 01:45:56 +0600

    firebase register user and onAuthChange state set completed

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,27 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../Firebase/firebase.config";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "hablu",
-    email: "hablu@.com",
-  });
-  const authData = { user, setUser };
+  const [user, setUser] = useState(null);
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  const authData = { user, setUser, createUser };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
--- src/pagees/Login/Register/Register.jsx
+++ src/pagees/Login/Register/Register.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const Register = () => {
+  const { createUser } = use(AuthContext);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, photo, email, password);
+    createUser(email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+
+        // ..
+      });
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <h1 className="text-center pt-8 font-bold text-xl">
           Register your account
         </h1>
-        <div className="card-body">
+        <form onSubmit={handleRegister} className="card-body">
           <fieldset className="fieldset">
             {/* name */}
             <label className="label font-bold">Your Name</label>
             <input
+              name="name"
               type="text"
               className="input"
               placeholder="Enter your name"
@@ -20,6 +46,7 @@ const Register = () => {
             {/* photo url */}
             <label className="label font-bold">Photo URL</label>
             <input
+              name="photo"
               type="text"
               className="input"
               placeholder="Enter your photo url"
@@ -27,17 +54,21 @@ const Register = () => {
             {/* email */}
             <label className="label font-bold">Email</label>
             <input
+              name="email"
               type="email"
               className="input"
               placeholder="Enter your email address"
             />
             <label className="label font-bold">Password</label>
             <input
+              name="password"
               type="password"
               className="input"
               placeholder="Enter your password"
             />
-            <button className="btn btn-neutral mt-4">Register</button>
+            <button type="submit" className="btn btn-neutral mt-4">
+              Register
+            </button>
             <p className="mt-6 text-center font-bold text-base-300">
               Already Have An Account ?{" "}
               <Link className="text-secondary" to="/auth/login">
@@ -46,7 +77,7 @@ const Register = () => {
               </Link>
             </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
