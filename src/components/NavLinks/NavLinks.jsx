7ee7da1cdec04944d9ed02a00960363759a6e0b8
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-04-12 15:14:54 +0600

    logout icon implemented

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -1,14 +1,16 @@
 import React, { use } from "react";
 import { Link, NavLink } from "react-router";
-import user from "../../assets/user.png";
+import userIcon from "../../assets/user.png";
 import "../../App.css";
 import { AuthContext } from "../../Provider/AuthProvider";
 
 const NavLinks = () => {
+  const { user } = use(AuthContext);
   return (
     <div className="flex flex-col md:flex-row gap-3 md:gap-0 py-2 max-w-11/12 mx-auto px-4  justify-between items-center">
       {/* Logo placeholder */}
-      <div className="hidden md:block" />
+      <div className="hidden" />
+      {user && user.email}
       {/* Nav links */}
       <nav className="flex gap-3 md:gap-6 text-sm md:text-base text-base-300 flex-wrap justify-center">
         <NavLink
@@ -45,13 +47,23 @@ const NavLinks = () => {
 
       {/* User actions */}
       <div className="flex items-center gap-2 md:gap-3">
-        <img src={user} alt="user avatar" className="w-8 h-8 rounded-full" />
-        <Link
-          to="/auth/login"
-          className="btn btn-primary text-white px-4 md:px-6 py-1.5 md:py-2 text-xs md:text-sm font-medium"
-        >
-          Login
-        </Link>
+        <img
+          src={userIcon}
+          alt="user avatar"
+          className="w-8 h-8 rounded-full"
+        />
+        {user ? (
+          <button className="btn btn-primary text-white px-4 md:px-6 py-1.5 md:py-2 text-xs md:text-sm font-medium">
+            Logout
+          </button>
+        ) : (
+          <Link
+            to="/auth/login"
+            className="btn btn-primary text-white px-4 md:px-6 py-1.5 md:py-2 text-xs md:text-sm font-medium"
+          >
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
